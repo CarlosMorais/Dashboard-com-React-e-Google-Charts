3dd2d41c7449fb343b1d07ae43a47b76b47fd5b8
Carlos Morais <br.carlosmorais@gmail.com>
2021-01-22 16:54:20 -0300

    up

---
+++ build/precache-manifest.211fb82f64189ddc013fed95c12f11f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dc640eff918043a3221909ce9867b45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51b3a606608a069879dd",
+    "url": "/static/css/main.13495408.chunk.css"
+  },
+  {
+    "revision": "12502ba3792857bd27ce",
+    "url": "/static/js/2.81221ce4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.81221ce4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "51b3a606608a069879dd",
+    "url": "/static/js/main.2faea5f4.chunk.js"
+  },
+  {
+    "revision": "fef0669c63847c5d06a0",
+    "url": "/static/js/runtime-main.e9665585.js"
+  }
+]);
